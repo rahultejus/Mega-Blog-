01d4d36d7ca77bec2a3f3969ff9a292d13956a66
rahultejus <rahultejus123@gmail.com>
2025-01-25 17:01:58 +0530

    header footer button and select dropdown completes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from "./appwrite/auth"
 import { login,logout } from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 function App() {
  const [loading,setLoading]=useState(true)
  const dispatch=useDispatch()
@@ -20,12 +23,18 @@ useEffect(()=>{
 },[])
 
 
- return loading?(null):(
- <div 
- className=
- 'min-h-screen flex flex-wrap'>
-
- </div>)
+ return !loading ? (
+  <div className=
+  'min-h-screen flex flex-col content-between bg-gray-400'
+  >
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        TODO:<Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div>) : null;
 }
 
 export default App
