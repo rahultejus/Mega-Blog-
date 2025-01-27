8e5468aaca002fc5bdd4f6a44dd24e6de2170a23
rahultejus <rahultejus123@gmail.com>
2025-01-27 18:26:10 +0530

    implementing login signup component and a real time editor

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,21 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Login,
+    Signup,
+    PostCard,
+    PostForm,
 }  
